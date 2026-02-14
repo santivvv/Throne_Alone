09c2a2b697dcf1f78f67b019276d1866c9a01e03
santi <santiago.voulgaris@gmail.com>
2026-02-14 10:35:18 -0500

    game loop

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pygame
+import sys
+
 pygame.init()
 
 screen = pygame.display.set_mode((1920, 1080))
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
